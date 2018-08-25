0d6e2999bbe8743e03895082fd6a39774f6f448f
Chase Hamby <c.chase.hamby@gmail.com>
2018-08-25 10:26:41 -0500

    object shorthand notation

--- main.js
+++ main.js
@@ -5,12 +5,19 @@ let counter = 0;
 counter = counter +1;
 counter += 1;
 counter ++
-
 //counter = 3
+
 console.log('counter: ', counter);
 
 const greetingElement = document.getElementById('greeting');
 const firstName = 'Chase';
 const lastName = 'Hamby';
-
 greetingElement.innerHTML = `Hello my name is ${firstName} ${lastName}`;
+
+//object shorthand notation
+let name = 'callan';
+let age = '100000';
+
+let person = {name, age};
+
+console.log('person:', person.name);
