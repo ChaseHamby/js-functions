c1d2ac2da44238522cd95de505e0cb86922466e0
Chase Hamby <c.chase.hamby@gmail.com>
2018-08-25 12:23:53 -0500

    finished js challenge

---
+++ challenge.js
@@ -0,0 +1,15 @@
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += stringToPrint;
+};
+
+let bandNumber = 1;
+
+let takeNumber = function (bandName) {
+  const bandString = `<h3>${bandNumber}. ${bandName}</h3>`;
+  bandNumber++;
+  printToDom(bandString, 'band');
+};
+
+takeNumber("Galactic Scum"); // This should display "1. Galactic Scum" in the DOM
+takeNumber("Underdogs"); // This should display "2. Underdogs" in the DOM
