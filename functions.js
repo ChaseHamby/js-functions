cbc83f8c95e6c72c3a054e614c5556522edd184b
Chase Hamby <c.chase.hamby@gmail.com>
2018-08-25 11:48:28 -0500

    made writeToDom

--- functions.js
+++ functions.js
@@ -20,4 +20,21 @@ const bearSticks = nuggetizer('bear');
 
 console.log(nomnom('Chase', bearSticks)); // Chase devoured bear sticks
 
+// function called numberAdder that takes in a number and returns the number plus 3: so if input = 22, output = 25
 
+
+const numberAdder = (num) => {
+    const finalNumber = num + 3;
+    printToDom(`<h2>${finalNumber}</h2>`, 'allTheNumbers');
+};
+
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += stringToPrint;
+};
+
+numberAdder(22);
+numberAdder(2);
+numberAdder(7245);
+
+printToDom('Im ready for lunch', 'feelings');
